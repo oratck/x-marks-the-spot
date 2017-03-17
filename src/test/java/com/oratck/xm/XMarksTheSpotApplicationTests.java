a2561ee11275a37a75c46ee49a37e9ee089413ea
oratck <oratck@gmail.com>
2017-03-18 00:44:45 +0700

    Initial project

---
+++ src/test/java/com/oratck/xm/XMarksTheSpotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oratck.xm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XMarksTheSpotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
